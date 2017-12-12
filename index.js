489830f6e92df2c7e8aaac1b4ada754a70b76f7d
Jason Pompon <jasonpompon@gmail.com>
2017-12-12 04:08:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {}
 var updateObjectWithKeyAndValue = (object, key, value) => {
     return Object.assign({}, object, {[key]: value})
 }
+
+var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+    object[key] = value;
+    return object
+}
